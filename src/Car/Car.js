6a4a93a768b853c77929668069be7ee10159a6af
Dmytro Myronenko <18683578+miron809@users.noreply.github.com>
2021-01-13 16:00:26 +0200

    added radium

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Radium from "radium";
 import "./Car.css";
 
-export default (props) => {
+const Car = (props) => {
   const inputClasses = ["input"];
 
   props.name ? inputClasses.push("green") : inputClasses.push("red");
@@ -9,7 +10,12 @@ export default (props) => {
 
   const style = {
     border: "1px solid #ccc",
-    boxShadow: "0 4px 5px 0 rgba(0, 0, 0, 0.14)"
+    boxShadow: "0 4px 5px 0 rgba(0, 0, 0, 0.14)",
+    ":hover": {
+      border: "1px solid #aaa",
+      boxShadow: "0 4px 15px 0 rgba(0,0,0,0.25)",
+      cursor: "pointer"
+    }
   };
 
   return (
@@ -28,3 +34,5 @@ export default (props) => {
     </div>
   );
 };
+
+export default Radium(Car);
